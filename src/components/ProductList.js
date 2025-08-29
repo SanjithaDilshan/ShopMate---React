9e01645270328c83e2bd387add5b0a7e82268d27
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-08-29 15:05:16 +0530

    useCallback Hook

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 export const ProductList = () => {
     const [products, setProducts] = useState([]);
     const [url, setUrl] = useState("http://localhost:8000/products");
-    const [counter, setCounter] = useState(0);
+
+    const fetchProducts = useCallback(async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data);
+      },[url])
 
     useEffect(() => {
-      fetch(url)
-      .then(response => response.json())
-      .then(data => setProducts(data));
-    }, [url]);
+      fetchProducts();
+      console.log("-");
+    }, [fetchProducts]);
 
-    useEffect(() => {
-      console.log(counter);
-    }, [counter]);
-
-  return (
+    return (
     <section>
       <div className="filter">
-        <button onClick={() => setCounter(counter + 1)}>{counter}</button>
         <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock</button>
       </div>
